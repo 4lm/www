7e08e1918937e1f46906c95ed601453ae621516e
Alexis Michaltsis <alexis@michaltsis.net>
2019-07-23 13:30:20 +0200

    Add smooth scrolling & 0.9 navbar alpha on scroll

--- udemy/02_edge_ledger/js/main.js
+++ udemy/02_edge_ledger/js/main.js
@@ -11,3 +11,28 @@ function initMap() {
     // The marker, positioned at location
     const marker = new google.maps.Marker({ position: loc, map: map });
 }
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+    if (this.window.scrollY > 65) {
+        this.document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+        this.document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 65
+            },
+            800
+        );
+    }
+});
