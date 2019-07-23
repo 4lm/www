4fef5c123cd05081c3f0d7ed7e339f17b59d14c8
Alexis Michaltsis <alexis@michaltsis.net>
2019-07-23 14:05:41 +0200

    Add mobile view

--- udemy/02_edge_ledger/js/main.js
+++ udemy/02_edge_ledger/js/main.js
@@ -14,7 +14,7 @@ function initMap() {
 
 // Sticky menu background
 window.addEventListener('scroll', function() {
-    if (this.window.scrollY > 65) {
+    if (this.window.scrollY > 150) {
         this.document.querySelector('#navbar').style.opacity = 0.9;
     } else {
         this.document.querySelector('#navbar').style.opacity = 1;
@@ -30,7 +30,7 @@ $('#navbar a, .btn').on('click', function (event) {
 
         $('html, body').animate(
             {
-                scrollTop: $(hash).offset().top - 65
+                scrollTop: $(hash).offset().top - 100
             },
             800
         );
